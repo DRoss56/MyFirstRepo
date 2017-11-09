3e3685e6025a114ddae956bdbb6044e77d94abbf
DRoss56 <33524327+DRoss56@users.noreply.github.com>
2017-11-09 12:07:21 -0500

    Initial

---
+++ MadLIBS.py
@@ -0,0 +1,20 @@
+print ("Write a person")
+name1 = input()
+
+print("Write a type of injury")
+injury1 = input()
+
+print("Write a kind of body part")
+bodypart1 = input()
+
+answer = input ()
+
+
+### MAD LIB ###
+
+print ("Whenever " + name1 + " tries to square up with me, I "
+       + injury1 + " their " + bodypart1+ " to show that I am the dominant Male ")
+
+print("Pop quiz,Dashiell is in a room with the Rock,Shaq,The Hulk,and Wolverine- Who is the dominant male? A=Dashiell B=The Rock C=Shaq D=The hulk")
+
+
