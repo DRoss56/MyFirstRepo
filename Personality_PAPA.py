2abe19e03d34174465e585df1e4124d9ca1ca35b
DRoss56 <33524327+DRoss56@users.noreply.github.com>
2018-03-14 11:03:47 -0400

    Add files via upload

---
+++ Personality_PAPA.py
@@ -0,0 +1,14 @@
+name = " Dashiell"
+state = "Texas"
+tvshow = " The Office"
+
+print (name + " likes to watch" + tvshow)
+
+
+print ("Whats your favorite subject?")
+subject = input()
+
+if subject == "History":
+    print ("Thats right")
+else:
+        print (subject + "is the wrong subject")
